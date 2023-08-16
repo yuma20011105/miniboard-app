4ae21eeb54de0d4137136d46e9246e0e1df35e70
yuma <yuma1105howa@icloud.com>
2023-08-16 22:13:08 +0900

    コードの修正

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
+const db=require('../models/index');
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/',(req,res,next)=>{
+  db.User.findAll().then(usrs=>{
+    var data={
+      title:'Users/Index',
+      content:usrs
+    }
+    res.render('users/index',data);
+  });
+});
+
+router.get('/:userId',(req,res)=>{
+  res.send(req.params)
 });
 
 module.exports = router;
